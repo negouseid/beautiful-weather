c77776169e953faa26c58b28fd734b82572621bc
negouseid <negouseid@gmail.com>
2018-09-24 16:25:47 -0600

    update port

--- src/server.js
+++ src/server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname,'/dist/beautiful-forecast/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-server.listen(process.env.PORT || port)
+app.listen(process.env.PORT || 8080);
